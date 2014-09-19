dffc5902f9fe5ee11e60ae7fa417e666cdf8e836
soygcm <gcmelmejor@gmail.com>
2014-09-18 23:16:17 -0600

    Tarea terminada

--- Tarea4/str2list/BUnit.h
+++
@@ -1,7 +0,0 @@
-// BUnit.h (C) 2008  adolfo@di-mare.com
-
-#error http://www.di-mare.com/adolfo/p/BUnit.htm
-#error http://www.di-mare.com/adolfo/p/BUnit/BUnit.zip
-#error Falta el archivo BUnit.h
-
-// EOF: BUnit.h
